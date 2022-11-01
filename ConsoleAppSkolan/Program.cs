40ac3cfc2ecb89c8820f94dd2dbcf8cd699d51ac
olivier <olivier.ilunga@hotmail.com>
2022-11-01 14:19:42 +0100

    Run 2 methods

--- ConsoleAppSkolan/Program.cs
+++ ConsoleAppSkolan/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine($"Hello, World! {TimeOfThisDay()}");
+            Console.WriteLine($"Hello, World! {CurrentTime()}");
+
             WorkingOnLoops();
         }
         private static DateTime CurrentTime()
